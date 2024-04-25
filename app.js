a8df8bb06b22f833dbe1a0edbbab46d262b9eb27
My Name <adildotes@gmail.com>
2024-04-26 02:10:58 +0500

    add assignment and modified

---
+++ app.js
@@ -0,0 +1,26 @@
+let pEl = document.querySelector("p");
+
+function formatNumber(number) {
+    const numString = number.toString();
+    let result = '';
+
+    for (let i = 0; i < numString.length; i++) {
+        if (i % 2 === 0) {
+            result += numString[i];
+        } else {
+            const currentDigit = parseInt(numString[i]);
+            const previousDigit = parseInt(numString[i - 1]);
+            if (currentDigit % 2 === 1 && previousDigit % 2 === 1) {
+                result += '-' + numString[i];
+            } else {
+                result += numString[i];
+            }
+        }
+    }
+    return result;
+}
+
+const num = prompt("Enter some numbers:");
+const formattedNum = formatNumber(num);
+
+pEl.textContent = formattedNum;
